6132288074c646c2a8e59369c4da7d64e4c16359
Lion Development <157517442+LionDev-FiveM@users.noreply.github.com>
2024-01-23 23:21:21 +0100

    Add files via upload

--- lionKeyboardLayout/server.lua
+++ lionKeyboardLayout/server.lua
@@ -2,28 +2,29 @@ Citizen.CreateThread(function()
     local vRaw = LoadResourceFile(GetCurrentResourceName(), 'version.json')
     if vRaw and Config.VersionCheck then
         local v = json.decode(vRaw)
-        local url = 'https://raw.githubusercontent.com/LionDev-FiveM/lionDRP/main/lionDRP/version.json'
+        local url = 'https://raw.githubusercontent.com/LionDev-FiveM/lionKeyboardLayout/main/lionKeyboardLayout/version.json'
         PerformHttpRequest(url, function(code, res)
             if code == 200 then
                 local rv = json.decode(res)
                 if rv.version ~= v.version then
                     print(([[
-_      _               _____                 _                                  _   
+ _      _               _____                 _                                  _   
 | |    (_)             |  __ \               | |                                | |  
 | |     _  ___  _ __   | |  | | _____   _____| | ___  _ __  _ __ ___   ___ _ __ | |_ 
 | |    | |/ _ \| '_ \  | |  | |/ _ \ \ / / _ \ |/ _ \| '_ \| '_ ` _ \ / _ \ '_ \| __|
 | |____| | (_) | | | | | |__| |  __/\ V /  __/ | (_) | |_) | | | | | |  __/ | | | |_ 
 |______|_|\___/|_| |_| |_____/ \___| \_/ \___|_|\___/| .__/|_| |_| |_|\___|_| |_|\__|
-                                                    | |                             
-                                                    |_|                             
-Script: lionDRP
+                                                     | |                             
+                                                     |_|                             
+Script: lionKeyboardLayout
 UPDATE: %s AVAILABLE
 CHANGELOG: %s
+Download: https://github.com/LionDev-FiveM/lionKeyboardLayout
 If you need help, join our Discord: https://dsc.gg/lion-dev
 ]]):format(rv.version, rv.changelog))
                 end
             else
-                print('nearest-postal was unable to check the version')
+                print('Error in versioncheck')
             end
         end, 'GET')
     end
